4050cf3aa5cc66be78718cbeaa0d77c21a72f9ee
Armin Mirreai <arminmirrezai@gmail.com>
2020-07-13 00:25:02 +0200

    last push

--- NN_estimator.py
+++ NN_estimator.py
@@ -151,7 +151,7 @@ def split_data(X, y, num_sim):
     #         y_test_list; list with 5 folds of testing validating data 
 
     # Set random state to 1 for reproducable results
-    kfold = KFold(5, shuffle=True, random_state=1)
+    kfold = KFold(10, shuffle=True, random_state=1)
     X_train_list = []
     y_train_list = []
     X_test_list = []
@@ -165,56 +165,56 @@ def split_data(X, y, num_sim):
     return X_train_list, y_train_list, X_test_list, y_test_list
 
 
-def accuracy_visual(predictions, true_values, num_sim, split):
+def accuracy_visual(predictions, split):
     # Summary: this function plots the predictions and real values of a data set
     # Input: predictions; parameter predictions
-    #        true_values; real parameter values
     #        num_sim; number of simulations
     #        split; number of observations in validating data
 
-    rho_pred = np.array(predictions[:, 0])
-    s2_pred = np.array(predictions[:, 1])
-    alpha_pred = np.array(predictions[:, 2])
-    beta_pred = np.array(predictions[:, 3])
-    delta_pred = np.array(predictions[:, 4])
-    nu_pred = np.array(predictions[:, 5])
+    split = split-3
 
-    rho_true = np.array(true_values[:, 0])
-    s2_true = np.array(true_values[:, 1])
-    alpha_true = np.array(true_values[:, 2])
-    beta_true = np.array(true_values[:, 3])
-    delta_true = np.array(true_values[:, 4])
-    nu_true = np.array(true_values[:, 5])
+    rho_pred = np.sort(np.array(predictions[:, 0]))
+    s2_pred = np.sort(np.array(predictions[:, 1]))
+    alpha_pred = np.sort(np.array(predictions[:, 2]))
+    beta_pred = np.sort(np.array(predictions[:, 3]))
+    delta_pred = np.sort(np.array(predictions[:, 4]))
+    nu_pred = np.sort(np.array(predictions[:, 5]))
 
     fig, ax = plt.subplots(nrows=2, ncols=3)
 
-    x1 = np.linspace(0, 0.1, num=split)
-    x2 = np.linspace(0, 3, num=split)
-    x3 = np.linspace(0.3, 0.4, num=split)
-    x4 = np.linspace(0.9, 1.0, num=split)
-    x5 = np.linspace(0.0, 0.1, num=split)
-    x6 = np.linspace(0, 3, num=split)
+    x1 = np.linspace(0, 1, num=split-3)
+    x2 = np.linspace(0, 3, num=split-3)
+    x3 = np.linspace(0.3, 0.4, num=split-3)
+    x4 = np.linspace(0.9, 1.0, num=split-3)
+    x5 = np.linspace(0.0, 0.1, num=split-3)
+    x6 = np.linspace(0, 3, num=split-3)
 
-    ax[0, 0].scatter(x1, rho_pred, c='black')
-    ax[0, 1].scatter(x2, s2_pred, c='black')
-    ax[0, 2].scatter(x3, alpha_pred, c='black')
-    ax[1, 0].scatter(x4, beta_pred, c='black')
-    ax[1, 1].scatter(x5, delta_pred, c='black')
-    ax[1, 2].scatter(x6, nu_pred, c='black')
+    ax[0, 0].scatter(x1, rho_pred[3:split], c='black')
+    ax[0, 1].scatter(x2, s2_pred[3:split], c='black')
+    ax[0, 2].scatter(x3, alpha_pred[3:split], c='black')
+    ax[1, 0].scatter(x4, beta_pred[3:split], c='black')
+    ax[1, 1].scatter(x5, delta_pred[3:split], c='black')
+    ax[1, 2].scatter(x6, nu_pred[3:split], c='black')
 
-    ax[0, 0].plot(x1, rho_true, '-')
-    ax[0, 1].plot(x2, s2_true, '-')
-    ax[0, 2].plot(x3, alpha_true, '-')
-    ax[1, 0].plot(x4, beta_true, '-')
-    ax[1, 1].plot(x5, delta_true, '-')
-    ax[1, 2].plot(x6, nu_true, '-')
+    ax[0, 0].plot(x1, x1, '-')
+    ax[0, 1].plot(x2, x2, '-')
+    ax[0, 2].plot(x3, x3, '-')
+    ax[1, 0].plot(x4, x4, '-')
+    ax[1, 1].plot(x5, x5, '-')
+    ax[1, 2].plot(x6, x6, '-')
 
-    ax[0, 0].set_title('rho')
-    ax[0, 1].set_title('s2')
-    ax[0, 2].set_title('alpha')
-    ax[1, 0].set_title('beta')
-    ax[1, 1].set_title('delta')
-    ax[1, 2].set_title('nu')
+    ax[0,0].set_xlabel('true rho')
+    ax[0,0].set_ylabel('estimated rho')
+    ax[0,1].set_xlabel('true s2')
+    ax[0,1].set_ylabel('estimated s2')
+    ax[0,2].set_xlabel('true alpha')
+    ax[0,2].set_ylabel('estimated alpha')
+    ax[1,0].set_xlabel('true beta')
+    ax[1,0].set_ylabel('estimated beta')
+    ax[1,1].set_xlabel('true delta')
+    ax[1,1].set_ylabel('estimated delta')
+    ax[1,2].set_xlabel('true nu')
+    ax[1,2].set_ylabel('estimated nu')
 
     fig.tight_layout()
     plt.show()
@@ -235,7 +235,7 @@ def accuracy_statistics(predictions, true_values, split):
     bias_to_mean_list = []
 
     # Fill lists
-    for i in range(split):
+    for i in range(3, split-3):
         bias_list.append(true_values[i] - predictions[i])
         bias_to_mean_list.append(true_values[i] - mean)
 
@@ -252,8 +252,8 @@ def accuracy_statistics(predictions, true_values, split):
         squared_bias_to_mean, axis=0, dtype='float64')
 
     # Apply final calculations
-    average_bias = np.divide(aggregate_bias, split)
-    average_squared_bias = np.divide(aggregate_squared_bias, split)
+    average_bias = np.divide(aggregate_bias, split-6)
+    average_squared_bias = np.divide(aggregate_squared_bias, split-6)
     RMSE = np.sqrt(average_squared_bias)
     predictivity = (aggregate_squared_bias_to_mean -
                     aggregate_squared_bias)/aggregate_squared_bias_to_mean
@@ -263,7 +263,7 @@ def accuracy_statistics(predictions, true_values, split):
     print("\nPredictivity: ", predictivity)
 
 
-def neural_network(X_train, y_train, X_test, num_SS):
+def neural_network(X_train, y_train, X_test, y_test, num_SS):
     # Summary: this function trains a neural network and returns it's predictions
     # Input: X_train; list with 5 folds of training data 
     #        y_train; list with 5 folds of training validating data
@@ -274,24 +274,30 @@ def neural_network(X_train, y_train, X_test, num_SS):
     # Set up neural network
     model = keras.Sequential()
     model.add(layers.Dense(50, input_dim=num_SS, activation='relu'))
-    model.add(layers.Dense(10, activation='relu'))
-    model.add(layers.Dense(10, activation='relu'))
-    model.add(layers.Dense(10, activation='relu'))
+    model.add(layers.Dense(50, activation='relu'))
+    model.add(layers.Dense(50, activation='relu'))
+    model.add(layers.Dense(50, activation='relu'))
+    model.add(layers.Dense(50, activation='relu'))
     model.add(layers.Dense(6, activation='relu'))
 
     # Specify settings
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae', tf.keras.metrics.RootMeanSquaredError()])
     es = EarlyStopping(monitor='loss', patience=10)
 
+
+    scores = []
     # Train neural network
     for i in range(1, len(X_train)):
         model.fit(X_train[i], y_train[i], epochs=10000,
-                  batch_size=5, callbacks=[es])
+                  batch_size=5, callbacks=[es], verbose=0)
+
+        scores.append(model.evaluate(X_test[i], y_test[i], verbose=0))
 
     # Predict on testing data
-    predictions = model.predict(X_test[0])
+    # predictions = model.predict(X_test[0])
 
-    return predictions
+    return np.mean(np.array(scores), axis=0)
+    # return predictions
 
 
 def elastic_net(X_train, y_train, X_test):
@@ -315,9 +321,10 @@ def elastic_net(X_train, y_train, X_test):
 
 def main():
     # Choose number of data points you want to simulate
-    T = 50
+    T = 200
     # Set seed
     np.random.seed(1337)
+    # tf.random.set_seed(1337)
     # Define lists
     par = []
     par_list = []
@@ -325,26 +332,38 @@ def main():
     SS_list = []
 
     # Set number of simulations
-    power = 4
-    num_sim = 10**power
+    num_sim = 1000
 
     # Set whether to calculate squares and crossterms of summary statistics
-    # Set to 'True' when fitting elastic net and 'False' when fitting neural network
-    crossterms_SS = True
+    crossterms_SS = False
+
+    # Generate random parameter values
+    rho = np.random.uniform(0, 1, num_sim)
+    s2 = np.random.uniform(0, 3, num_sim)
+    alpha = np.random.uniform(0.3, 0.4, num_sim)
+    beta = np.random.uniform(0.9, 1, num_sim)
+    delta = np.random.uniform(0, 0.1, num_sim)
+    nu = np.random.uniform(0, 3, num_sim)
+
+    # Sort the parameter values
+    # rho = np.sort(rho)
+    # s2 = np.sort(s2)
+    # alpha = np.sort(alpha)
+    # beta = np.sort(beta)
+    # delta = np.sort(delta)
+    # nu = np.sort(nu)
 
     for i in range(0, num_sim):
         # Choose a parameter vector you want to simulate data for
-        par.append({"rho": 0+i*10**(-power), "s2": 0+3*i*10**(-power), "alpha": 0.3 +
-                    i*10**(-power-1), "beta": 0.9+i*10**(-power-1), "delta": 0+i*10**(-power-1), "nu": 0+3*i*10**(-power)})
+        par.append({"rho": rho[i], "s2": s2[i], "alpha": alpha[i], "beta": beta[i], "delta": delta[i], "nu": nu[i]})
 
         # Call function for simulation
         data.append(SimulateData(par[i], T))
 
         # Compute summary statistics
-        if i > 0:
-            SS_list.append(extract_summary_statistics(data[i], crossterms_SS))
-            par_list.append(list(par[i].values()))
-
+        SS_list.append(extract_summary_statistics(data[i], crossterms_SS))
+        par_list.append(list(par[i].values()))
+        
     # Convert list to array
     X = np.array(SS_list)
     y = np.array(par_list)
@@ -353,14 +372,37 @@ def main():
     X_train, y_train, X_test, y_test = split_data(X, y, num_sim)
     num_SS = np.size(X, 1)
 
-    predictions_nn = neural_network(X_train, y_train, X_test, num_SS)
+    # Run repeated evaluation experiment
+    score_list = []
+    for i in range(0,100):
+        score = neural_network(X_train, y_train, X_test, y_test, num_SS)
+        score_list.append(score)
+
+    # Calculate confidence interval
+    score = np.array(score_list)
+    mean_score = np.mean(score, axis=0)
+    std_dev = np.std(score, axis=0)
+    std_err = std_dev / np.sqrt(np.size(score, axis=0))
+    interval = 1.96 * std_err
+    lower_interval = mean_score - interval
+    upper_interval = mean_score + interval
+
+    print("score:", score)
+    print("mean:", mean_score)
+    print("std_dev:", std_dev)
+    print("std_err:", std_err)
+    print("interval:", interval)
+    print("lower_bound:", lower_interval)
+    print("upper_bound:", upper_interval)
+
+    # predictions_nn = neural_network(X_train, y_train, X_test, y_test, num_SS)
     # predictions_el = elastic_net(X_train[0], y_train[0], X_test[0])
 
-    accuracy_statistics(predictions_nn, y_test[0], np.size(y_test[0], axis=0))
+    # accuracy_statistics(predictions_nn, y_test[0], np.size(y_test[0], axis=0))
     # accuracy_statistics(np.array(predictions_el).T, y_test[0], np.size(y_test[0], axis=0))
 
-    accuracy_visual(predictions_nn, y_test[0], num_sim, np.size(y_test[0], axis=0))
-    # accuracy_visual(np.array(predictions_el).T, y_test[0], num_sim, np.size(y_test[0], axis=0))
+    # accuracy_visual(predictions_nn, y_test[0], np.size(y_test[0], axis=0))
+    # accuracy_visual(np.array(predictions_el).T, y_test[0], np.size(y_test[0], axis=0))
 
 
 if __name__ == "__main__":
